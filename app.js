7d92122df166e2744b775ebbe0703d84f9cdf9d5
unknown <timwuchia@gmail.com>
2023-08-05 21:28:14 -0400

    fix cors error

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const cors = require('cors');
 const axios = require('axios');
 
 app.use(cors())
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 //Initialize middleware
 app.use(express.json({extended: false}));
